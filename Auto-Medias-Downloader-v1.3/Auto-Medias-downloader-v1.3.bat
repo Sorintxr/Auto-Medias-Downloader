33793eae24225b2e851990e69bdd632d2c33848a
GauGoth-Corp <ggaugoth@gmail.com>
2025-10-11 19:09:25 +0200

    Remove .gitignore, update REAMDE.md to remove a pargraph problem, update the source bat and therefore the executable.

--- Auto-Medias-Downloader-v1.3/Auto-Medias-downloader-v1.3.bat
+++ Auto-Medias-Downloader-v1.3/Auto-Medias-downloader-v1.3.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDxVWSONKGOGIrof/eX+4f6UnnItRu04ao7W17WaKe8d1mflYZUl6l1TjOMZDlZKewKkfQYgpnxbikWQI8jRngrlQ0maq28/DGt4iGLUiTkHQcZ8lY4u1iS3/V6yu6oH33TwX60LBy73kOI7BMgN+Vv6O3XblaZHSfP9V7zkB3vBIGYRwnnAhOM=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZkg0
+::ZQ05rAF9IBncCkqN+0xwdVsFAlfi
+::ZQ05rAF9IAHYFVzEqQIRPQ9bDCmHOGK7FfU45//14+WGpkgPNA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDCWXKGT6K7AY4enor86ItUMRW+M7fZ27
+::fBEirQZwNQPfEVWB+kM9LVsJDCWXKGT6K7AY4enor86ItUMRW+M7fZ27
+::cRolqwZ3JBvQF1fEqQIRPQ9bDCmHOGK7FfU45//14+WGpkgPFOssOI6byruZJfIV/0mkeJMp33tJ2M8DGxVUcROvZwEz6XpNuWjLVw==
+::dhA7uBVwLU+EWH6F4WU/PBMUbwuQLCXa
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE100gMQldSwyWfMlzRudMuru2vbrV9w06VfcYd5vTmpeBMvBdqmToedY/331SjNhMHh5LewC9awtU
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDxVWSONKGOGIrof/eX+4f6UnnItRu04ao7W17WaKe8d1mflYZUl6l1TjOMZDlZKewKkfQYgpnxbikWQI8jRngrlQ0maq28/DGt4iGLUiTkHQcZ8lY4u1iS3/V6yu6oH33TwX60LBy73kOI7BOgV6QX5RUzMk7JGB/LhfLPlDDfMJntSmiGN2M1vhoZN
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 REM Active le delayed expansion pour permettre l'expansion des variables dans les boucles
 setlocal enabledelayedexpansion
